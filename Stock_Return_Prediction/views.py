6a8fd7b96599e52149c105b2b30c0d760440226d
kush789 <singh.kushagra.1996@gmail.com>
2015-05-03 15:39:45 +0530

    Populated NASDAQ and NIKKEI databases.

--- Stock_Return_Prediction/views.py
+++ Stock_Return_Prediction/views.py
@@ -4,21 +4,7 @@ from django.http import HttpResponseRedirect
 from index.models import *
 import os
 module_dir = os.path.dirname(__file__)
-file_path = os.path.join(module_dir, 'DOWJONES.csv')
+file_path = os.path.join(module_dir, 'NIKKEI.csv')
 
 def index(request):
-	# g = open(file_path, "r")
-	# num = 0
-	# a = g.readlines()[1:][::-1]
-
-	# for i in range(len(a)):
-	# 	a[i] = a[i].strip("\n")
-	# 	a[i] = a[i].split(",")
-	# 	sample = DOWJONES(dowjonesid = num, value = float(a[i][1]), date = a[i][0])
-	# 	sample.save()
-	# 	num += 1
-	# 	print num
-	# # g.close()	
-	# for i in DOWJONES.objects.all():
-	# 	print i.dowjonesid, i.value, i.date
 	return render_to_response('index.html')
